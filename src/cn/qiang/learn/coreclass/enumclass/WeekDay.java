ed004542c073ebb034b1e3493b8695d4c8c16333
baiyang <baiyang@ainemo.com>
2020-01-06 07:51:17 +0800

    exception

---
+++ src/cn/qiang/learn/coreclass/enumclass/WeekDay.java
@@ -0,0 +1,9 @@
+package cn.qiang.learn.coreclass.enumclass;
+
+/**
+ * Created by fiona on 2020/1/5.
+ */
+enum  WeekDay {
+    MON, TUE, WED, THU, FRI, SAT, SUN;
+
+}
---
+++ src/cn/qiang/learn/coreclass/enumclass/WeekDay1.java
@@ -0,0 +1,14 @@
+package cn.qiang.learn.coreclass.enumclass;
+
+/**
+ * Created by fiona on 2020/1/5.
+ */
+enum WeekDay1 {
+
+//    public final int dayValue ;
+
+//    private WeekDay1(int dayValue,String chinese){
+//        this
+//    }
+
+}
