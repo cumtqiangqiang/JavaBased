4eca30871c98032e5ba6a66e0bb127e20b55147e
qqiang <cumtqiangqiang@163.com>
2021-03-05 10:35:07 +0800

    CollectorLearn

--- src/java/com/newfeature/stream/CollectorLearn.java
+++ src/java/com/newfeature/stream/CollectorLearn.java
@@ -33,11 +33,23 @@ public class CollectorLearn {
                 Collectors.toSet()));
 
 
-        List<Student> stus = Arrays.asList(new Student(21,"Beijing"),new Student(23,"Beijing"),new Student(34,"S" +
-                "Shanxi"),new Student(19,"Shanxi"));
-
-        stus.stream().collect(Collectors.reducing())
-
+//        List<Student> stus = Arrays.asList(new Student(21,"Beijing"),new Student(23,"Beijing"),new Student(34,"S" +
+////                "Shanxi"),new Student(19,"Shanxi"));
+////
+////        stus.stream().collect(Collectors.reducing())
+        System.out.println("======Reduce========");
+        Stream<String> streamStr = Arrays.asList("you", "give", "me", "stop").stream();
+        Stream<Integer> streamInt = Arrays.asList(1, 2, 3, 4, 5).stream();
+//        Integer result = streamInt.reduce(Integer::sum).get();
+//        System.out.println(result);
+//        String s1 = streamStr.reduce((a, b) -> a + "," + b).get();
+//        System.out.println(s1);
+//        String hello = streamStr.reduce("hello", (a, b) -> a + "," + b);
+//        System.out.println(hello);
+        String hello = streamStr.reduce("hello", (a, b) -> a + ":" + b, (r1, r2) -> null);
+        System.out.println(hello);
+//        Integer reduce = streamInt.reduce(1, (a, b) -> a + b);
+//        System.out.println(reduce);
 
 
     }
